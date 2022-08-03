61cc541e1b68a93dee6ac60adcec11091c585158
Rafael Delboni <rafadelboni@gmail.com>
2022-08-03 20:40:21 -0300

    fix: Adds layout comments

--- config.h
+++ config.h
@@ -15,5 +15,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 #define IGNORE_MOD_TAP_INTERRUPT
--- keymap.c
+++ keymap.c
@@ -17,43 +17,51 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-
-    KC_CTLA, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_RCSC,
-
-    KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RSSH,
-
-                               KC_GUBS, KC_LSP1,         KC_LEN2, KC_AESC
+  //,--------------------------------------------.                   ,--------------------------------------------.
+         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+      KC_CTLA,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_RCSC,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+      KC_LSHZ,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RSSH,
+  //|--------+--------+--------+--------+--------|--------| |--------+--------+--------+--------+--------+--------|
+                                          KC_GUBS, KC_LSP1,   KC_LEN2, KC_AESC
+                                      //`-----------------' `-----------------'
   ),
 
   [1] = LAYOUT(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-
-    KC_TAB,  KC_TRNS,  KC_GRV, KC_TILD,  KC_DEL,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-
-    KC_TRNS, KC_TRNS, KC_CEDL, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
-
-                               KC_TRNS, KC_TRNS,         MO(3),   KC_TRNS
+  //,--------------------------------------------.                   ,--------------------------------------------.
+         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+       KC_TAB,  KC_TRNS, KC_GRV, KC_TILD,  KC_DEL,                     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_CEDL, KC_BTN1, KC_BTN2,                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
+  //|--------+--------+--------+--------+--------|--------| |--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS, KC_TRNS,     MO(3), KC_TRNS
+                                      //`-----------------' `-----------------'
   ),
 
   [2] = LAYOUT(
-    KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-
-    KC_TRNS, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY,         KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE,
-
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR,         KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_TRNS,
-
-                               KC_TRNS, MO(3),           KC_TRNS, KC_TRNS
+  //,--------------------------------------------.                   ,--------------------------------------------.
+      KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+      KC_TRNS, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY,                     KC_MINS,  KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR,                     KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_TRNS,
+  //|--------+--------+--------+--------+--------|--------| |--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS,   MO(3),   KC_TRNS, KC_TRNS
+                                      //`-----------------' `-----------------'
   ),
 
   [3] = LAYOUT(
-    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,
-
-                               KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS
+  //,--------------------------------------------.                   ,--------------------------------------------.
+        RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+  //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------|--------| |--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS
+                                      //`-----------------' `-----------------'
   ),
 };
 
