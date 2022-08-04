3aaaa0e18e711828553810f985048a57332a31ba
Rafael Delboni <rafadelboni@gmail.com>
2022-08-04 14:10:14 -0300

    fix: Better placement for acents

--- keymap.c
+++ keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                   ,--------------------------------------------.
          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
-       KC_TAB,  KC_TRNS, KC_GRV, KC_TILD,  KC_DEL,                     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT,
+       KC_TAB,  KC_GRV, KC_TILD, KC_QUOT,  KC_DEL,                     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_TRNS,
   //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
       KC_TRNS, KC_TRNS, KC_CEDL, KC_BTN1, KC_BTN2,                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
   //|--------+--------+--------+--------+--------|--------| |--------+--------+--------+--------+--------+--------|
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
       KC_TRNS, KC_TRNS, KC_VOLU, KC_MNXT, KC_MPLY,                     KC_MINS,  KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE,
   //|--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------|
-      KC_TRNS, KC_TRNS, KC_VOLD, KC_MPRV, KC_PSCR,                     KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_VOLD, KC_MPRV, KC_PSCR,                     KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS,
   //|--------+--------+--------+--------+--------|--------| |--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,   MO(3),   KC_TRNS, KC_TRNS
                                       //`-----------------' `-----------------'
