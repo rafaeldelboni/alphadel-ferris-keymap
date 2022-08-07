c8a54adfb39a8c4442c87f0da2de4448288574a1
Rafael Delboni <rafadelboni@gmail.com>
2022-08-07 01:04:51 -0300

    fix: Enables Permisive hold

--- config.h
+++ config.h
@@ -16,4 +16,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
