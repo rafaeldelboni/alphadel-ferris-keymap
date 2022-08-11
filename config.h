04bc40c32b8f59aa4ef4e5c7c6d815e9e3215069
Rafael Delboni <rafadelboni@gmail.com>
2022-08-11 00:02:54 -0300

    feat: Configs to better hold and tapping term

--- config.h
+++ config.h
@@ -15,6 +15,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 150
+#define TAPPING_TERM 170
 #define PERMISSIVE_HOLD
+// Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD 
