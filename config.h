124c5ac82215f6ecbda4053fb651b47c6004a78e
Rafael Delboni <rafadelboni@gmail.com>
2022-08-12 09:33:58 -0300

    feat: Faster tapping term

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 170
+#define TAPPING_TERM 160
 #define PERMISSIVE_HOLD
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
